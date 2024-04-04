10688defa6c0e451949a1bb21f9d2f8873b69197
TerlaN2233 <95983928+TerlaN2233@users.noreply.github.com>
2024-04-04 17:21:59 +0300

    deleted unnecessary

--- pages/admin/category/categoryMoxkData.tsx
+++
@@ -1,6 +0,0 @@
-const categoryMoxkData = [
-  { Id: "00001", Name: "pizza", Slug: "pizza", img: "img" },
-  { Id: "00002", Name: "fast-food", Slug: "fast-food", img: "img" },
-  { Id: "00003", Name: "coffee", Slug: "coffee", img: "img" },
-];
-export default categoryMoxkData;
